21427eb1a218ea37dc7902c9cff0a4d15833f8b4
Erik Hanchett <ehhanche@amazon.com>
2024-01-26 11:24:02 -0800

    base

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+</script>
+
+<template>
+  <h1>Hello From root</h1>
+  <Authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </Authenticator>
+</template>
